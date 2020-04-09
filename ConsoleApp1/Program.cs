2c0d952f971c98300c9ee0075b42b059549d7499
josebarbo <33163895+josebarbo@users.noreply.github.com>
2020-04-09 08:04:44 +0100

    primer commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("exemplo ne usa github");
+            Console.ReadKey();
         }
     }
 }
